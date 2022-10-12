1677394e9e4c1287f976b97ad3ee9712d9a194d9
leo <leoking02@foxmail.com>
2022-10-12 09:08:10 +0800

    “ssss”

---
+++ libpng-1.6.35_extern_app/mymathApp.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "png.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+//    double a = add(1.1, 1.1);
+// int b =  version( );
+//    cout << "1.1加1.1等于" << a <<endl;
+    cout << "PNG_LIBPNG_VER_STRING = " << PNG_LIBPNG_VER_STRING <<endl;
+    return 0;
+}
